df3d468c386c43626fd9a432940206f2ca0f686f
Sander69 <>
2026-05-06 13:32:44 +0000

    Challenge: Working with Instance and Class Variables

---
+++ section/challenge_working_with_instance_and_class_variables/main.py
@@ -0,0 +1,13 @@
+class User:
+    total_users = 0
+
+    def __init__(self, username):
+        pass
+
+    def get_info(self):
+        pass
+
+user1 = User("alice")
+user2 = User("bob")
+print(user1.get_info())
+print(user2.get_info())
